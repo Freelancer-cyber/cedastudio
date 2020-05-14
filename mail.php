d8013138764e41870dcff2abbfd8bfcf4aab5cd6
ifoy03 <marcu.sorin.2003@gmail.com>
2020-05-14 13:22:45 +0300

    Update mail.php

--- mail.php
+++ mail.php
@@ -6,6 +6,6 @@
 	$subject= "=?utf-8?B?".base64_encode($services)."?=";
 	$headers= "Name: $name\r\nNumber: $phone\r\nContent-type: text/html; charset=utf-8\r\n";
 
-	$success = mail("info@cedastudio.com",$subject,$headers);
+	$success = mail("freelancer.cyber@gmail.com",$subject,$headers);
 	echo success;
 ?>
